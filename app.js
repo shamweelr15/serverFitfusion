1ab74dda1fa1b99aaee28a83275862107a7372fc
shamweelr15 <shamweelraza15@gmail.com>
2023-12-07 03:08:18 +0530

    changed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 
 app.use(express.json());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ||5000;
 
 app.use(session({
     secret: 'FTSCS',
